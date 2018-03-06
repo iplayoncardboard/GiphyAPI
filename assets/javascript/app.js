6e93205b248027727a23cc561b29f1c2f256d397
Erik C Hurst <ehurst01@gmail.com>
2018-03-05 20:17:58 -0700

    Completed API call. Added functionality to add a button from input text value.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,21 @@
 
 
-const apiKey = '4RVECha85B9XgKqImgh8uzjqSj9NcdL6'
-let apiURL = 'https://api.giphy.comGET /v1/gifs/search?api_key:'+apiKey+'&q:';
-let topics =["Game Boy", "Nintendo", "Dungeons and Dragons"]
+
+const topics =["Game Boy", "Nintendo", "Dungeons and Dragons","He-Man"]
 
 
 renderButtons();
 
-function search(){$.ajax(
+function search(item){
+    const apiKey = '4RVECha85B9XgKqImgh8uzjqSj9NcdL6';
+    let apiURL = 'https://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&q='+item+'&limit=10';
+    $.ajax(
     {url:apiURL,
         method: 'GET'
     }
 ).done(function(response){
 
+    displayGifs(response);
 });
 }
 
@@ -30,8 +33,19 @@ function renderButtons(){
 
 }
 
+function displayGifs(json){
+    console.log(json);
+}
 
 $(document).on('click', '.topic-button', function(event){
-    
+    searchTerm = $(this).attr('data-search');
+    search(searchTerm);
 
+});
+
+$('.submit-button').on('click', function(event){
+    event.preventDefault();
+    let gif = $('#search-box').val().trim();
+    topics.push(gif);
+    renderButtons();
 });
