c186a7ce0d6885be9147f4439e773f180fd01f83
Erik C Hurst <ehurst01@gmail.com>
2018-03-09 16:36:53 -0700

    finished layout added rating restriction and awesome 90s theme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,25 @@
 
 
-const topics =["Game Boy", "Nintendo", "Dungeons and Dragons","He-Man"]
+const topics =["Game Boy", "Nintendo", "Dungeons and Dragons","He-Man", "GI Joe", 'Thunder Cats', 'Teenage Mutant Ninja Turtles']
 
 
 renderButtons();
 
 function search(item){
     const apiKey = '4RVECha85B9XgKqImgh8uzjqSj9NcdL6';
-    let apiURL = 'https://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&q='+item+'&limit=10';
+    let apiURL;
+    let censodred = $('#pmrc').attr('class')
+    if(censodred==='naughty')
+        {
+        apiURL =  'https://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&q='+item+'&limit=10';
+    }
+
+    else{
+        apiURL =  'https://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&q='+item+'&limit=10&rating=pg';
+    }
+    
+   
+   
     $.ajax(
     {url:apiURL,
         method: 'GET'
@@ -24,7 +36,7 @@ function renderButtons(){
     topics.forEach(function(value,index){
         let btn = $('<button>');
         btn.text(value);
-        btn.addClass('btn btn-light topic-button mx-2');
+        btn.addClass('btn btn-light topic-button mx-2 my-1');
         btn.attr('data-search',value);
         btn.appendTo('.button-div');
     });
@@ -35,10 +47,13 @@ function renderButtons(){
 function displayGifs(json){
     console.log(json);
     $('#image-container').empty();
-    json.data.forEach(function(value, index){
-        $('<p>').text('Rated: '+ value.rating).addClass('img-fluid').appendTo('#image-container');
-        $('<img>').attr('src',value.images.fixed_width_small_still.url).addClass('pic').attr('data-still',value.images.fixed_width_small_still.url).attr('data-moving',value.images.fixed_width_small.url).attr('data-state','still').appendTo('#image-container');
-    });
+        json.data.forEach(function(value, index){
+            
+            let frame = $("<div>").addClass('col-4');
+            $('<p>').text('Rated: '+ value.rating).addClass('img-fluid').appendTo(frame);
+            $('<img>').attr('src',value.images.fixed_width_still.url).addClass('pic').attr('data-still',value.images.fixed_width_still.url).attr('data-moving',value.images.fixed_width.url).attr('data-state','still').appendTo(frame);
+            frame.appendTo('#image-container');
+        });
 }
 
 $(document).on('click', '.topic-button', function(event){
@@ -66,3 +81,20 @@ $(document).on('click','.pic', function(event){
     }
 
 });
+
+$('#pmrc').on('click', function(){
+    if($(this).attr('class')==='naughty'){
+    //change image to tipper gore.
+        $(this).attr('src', './assets/images/tipper.jpg');
+        //change class to nice.
+        $(this).toggleClass('naughty');
+        $(this).addClass('nice');
+    }
+    else{
+        // change img src to pwec,
+        $(this).attr('src', './assets/images/pwec.png');
+         //set class to naughty
+        $(this).toggleClass('nice');
+        $(this).addClass('naughty');
+    }
+    });
