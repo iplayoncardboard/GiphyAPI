35b0a3e6cbdbf119774264783430c5bb49e4e86a
Erik C Hurst <ehurst01@gmail.com>
2018-03-13 17:16:57 -0700

    Added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
+//Array to pre-generate buttons / hold new buttons.
 const topics =["Game Boy", "Nintendo", "Dungeons and Dragons","He-Man", "GI Joe",'Pearl Jam','Thunder Cats', 'Teenage Mutant Ninja Turtles','Nirvana']
 
-
+//Renders the buttons!
 renderButtons();
 
+//Search the Giphy API. Accepts a string to seach
 function search(item){
     const apiKey = '4RVECha85B9XgKqImgh8uzjqSj9NcdL6';
     let apiURL;
+    //Search results can be censored. This checks the state of the class to determine if the search should display PG13 and R
     let censodred = $('#pmrc').attr('class')
+    //Unrated Serach String
     if(censodred==='naughty')
         {
         apiURL =  'https://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&q='+item+'&limit=10';
     }
-
+    //Censored search string
     else{
         apiURL =  'https://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&q='+item+'&limit=10&rating=pg';
     }
@@ -21,12 +25,12 @@ function search(item){
         method: 'GET'
     }
 ).done(function(response){
-
+    //calls display gifs function (which display's gifs) and passes the object returned by the API as an argument. 
     displayGifs(response);
 });
 }
 
-
+//Renders buttons based on items in topics array. 
 function renderButtons(){
     $('.button-div').empty();
     topics.forEach(function(value,index){
@@ -40,6 +44,7 @@ function renderButtons(){
 
 }   
 
+//Displays gifs. Takes a JSON object from the Giphy API as an arugment.
 function displayGifs(json){
     console.log(json);
     $('#image-container').empty();
@@ -52,35 +57,43 @@ function displayGifs(json){
         });
 }
 
+//Event listener for topic buttons. When a button is clicked it will pas the value in the data-search parapater to the search function.
 $(document).on('click', '.topic-button', function(event){
     searchTerm = $(this).attr('data-search');
     search(searchTerm);
 
 });
 
+//Event listener for submit button to add a new topic / generate a new button.
 $('.submit-button').on('click', function(event){
     event.preventDefault();
+    //Ensures the box contains a value and the value is not one present in the array.
     if($('#search-box').val()&&!topics.includes($('#search-box').val())){
     let gif = $('#search-box').val();
     topics.push(gif);
+    //Calls the render buttons function to display the new button
     renderButtons();
+    //empties the search box
     $('#search-box').val("");
     }
 });
 
+//Event listener for gifs. When clicked they are motionless
 $(document).on('click','.pic', function(event){
     let state = $(this).attr('data-state');
+    //If they are still animate them 
     if(state==='still'){
         $(this).attr('src',$(this).attr('data-moving'));
         $(this).attr('data-state', 'moving');
     }
+    //or else turn off animations
     else{
         $(this).attr('src',$(this).attr('data-still'));
         $(this).attr('data-state', 'still');
     }
 
 });
-
+//Event listener for cencorship fenctionality
 $('#pmrc').on('click', function(){
     if($(this).attr('class')==='naughty'){
     //change image to tipper gore.
